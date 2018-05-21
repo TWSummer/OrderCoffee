b870ed7b7b473e34892c3d812903ffcc64aa38b0
Theodore Summer <TWSummer@gmail.com>
2018-05-21 16:07:04 -0700

    Layout HTML structure

---
+++ frontend/components/orders/order_item.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderItem = () => {
+  return (
+    <span>Order #100</span>
+  );
+};
+
+export default OrderItem;
--- frontend/components/orders/orders_index.jsx
+++ frontend/components/orders/orders_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import OrderItem from './order_item';
 
 class OrdersIndex extends React.Component {
   constructor(props) {
@@ -14,12 +15,52 @@ class OrdersIndex extends React.Component {
     this.props.fetchOrders(this.state.page, this.state.sortType, this.state.sort);
   }
 
+  getMonth() {
+    let now = new Date(Date.now());
+    return now.toDateString().split(" ")[1];
+  }
+
+  getDay() {
+    let now = new Date(Date.now());
+    return now.toDateString().split(" ")[2];
+  }
+
   render() {
     console.log(this.props);
     return (
       <section>
         <img src="https://i.imgur.com/EJMbREK.png" alt="logo"></img>
-        Hello Theo
+        <header>
+          <div className="today-date">
+            <span className="month">{this.getMonth()}</span>
+            <span className="day">{this.getDay()}</span>
+          </div>
+          <h1>Perfectly Ground Work Orders</h1>
+          <button>Create Order</button>
+        </header>
+        <main className="orders-table">
+          <header className="table-header">
+            <span className="blue-header">Orders</span>
+            <span className="column-labels">
+              <div>Coffee</div>
+              <div>Method</div>
+              <div>Quantity</div>
+              <div>Unit</div>
+              <div>Ship Date</div>
+              <div>Order</div>
+              <div>View</div>
+            </span>
+          </header>
+          <div>
+            {
+              this.props.orders.map((order) => {
+                return (
+                  <OrderItem></OrderItem>
+                );
+              })
+            }
+          </div>
+        </main>
       </section>
     );
   }

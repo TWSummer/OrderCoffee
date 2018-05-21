20f931b0ef8b649444e671c26793007299e892bd
Theodore Summer <TWSummer@gmail.com>
2018-05-20 20:15:35 -0700

    Add validations to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  validates :coffee_name, :method, :quantity, :unit,
+            :ship_date, presence: true
+  validates :coffee_name, inclusion: { in: ["Bella Donovan", "Giant Steps"] }
+  validates :method, inclusion: { in: ["Aeropress", "Coffee Maker",
+            "Cold Brew", "French Press", "Pour Over"] }
+  validates :quantity, :unit,
+            numericality: { only_integer: true, greater_than: 0 }
+  
 end
